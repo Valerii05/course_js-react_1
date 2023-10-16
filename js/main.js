4669a4ca90323d3b82539877533bb28b7b953114
Пользователь <valerii.footbolist@gmail.com>
2023-10-16 14:19:15 +0300

    create project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var a = 15;
+console.log(a);
+
+
+const name = "Valerii Bondarenko";
+console.log(name);
+
+const person = {
+	name: "Victor",
+	age: 12,
+	arr: [12,23,25],
+};
+
+person.age = 17;
+
+person.new = "mey";
+
+console.log(person.new);
+
+let question = confirm("Are you hare");
+console.log(question);
